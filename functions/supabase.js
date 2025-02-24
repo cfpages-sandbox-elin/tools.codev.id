28b3f2388c4ecc92275a859711a12e6a52f26cd3
achmadAndiDev <mas.achmadandi@gmail.com>
2025-02-24 09:18:02 +0700

    add config supabse

--- functions/supabase.js
+++ functions/supabase.js
@@ -1,8 +1,15 @@
 export async function onRequest(context) {
+    const allowedOrigins = ["https://tools.codev.id"];
+
+    const origin = context.request.headers.get("Origin");
+    if (!allowedOrigins.includes(origin)) {
+        return new Response("Forbidden", { status: 403 });
+    }
+
     return new Response(JSON.stringify({ 
         supabaseUrl: "https://izkhvitoxppvyssrlavq.supabase.co",
         supabaseKey: context.env.supabase_api_key 
     }), {
-        headers: { "Content-Type": "application/json" }
+        headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": origin }
     });
 }
